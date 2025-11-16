fc1c3a08e09e1a76ae6f2b27b1fcec9175152952
Paula Eduarda Justino <rpaulaeduarda@gmail.com>
2025-11-16 13:37:26 -0300

    Initial project setup - IncluiPay API

---
+++ api/src/test/java/com/incluipay/api/IncluiPayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incluipay.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncluiPayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
